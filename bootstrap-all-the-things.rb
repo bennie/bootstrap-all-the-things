451064f310fd4e60406616f3630a08b3cda0278d
Phillip Pollard <phil@crescendo.net>
2013-11-20 13:58:51 -0800

    Now running through the list.

--- bootstrap-all-the-things.rb
+++ bootstrap-all-the-things.rb
@@ -18,3 +18,34 @@ while not password
 	input2 = ask('Password Confirmation: ') { |q| q.echo = '*' };
 	password = input1 if input1 == input2
 end
+
+### Read in the input file
+
+check = Array.new
+
+File.open(filename).each do |line|
+	line.chomp
+
+	abort("Badly formatted line in file: #{line}") unless line =~ /^(.+?),(.+)$/
+
+	name = $1
+	fqdn = $2
+
+	command = 'echo knife bootstrap '       \
+			+ fqdn                     \
+			+ ' -N ' + name            \
+			+ ' --template macys.erb ' \
+			+ ' -x ' + username        \
+			+ ' -P ' + password        \
+			+ ' -r "role[base]" '      \
+			+ ' --sudo --use-sudo-password'
+
+	system(command)
+
+	ret = $?.exitstatus
+
+	warn "The bootstrap of node '#{name}' had an error. (Return code: #{ret})" if ret != 0
+
+	check.push(name).push(ret)	
+end
+
